bc2d19064fc2ca35e22ae6caf026882a7acb849f
Yann Duran <yd@live.com.au>
2019-09-21 00:33:23 +1000

    Add CommandsToVisibleCountString converter

---
+++ src/ui/Converters/CommandsToVisibleCountString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace StartPagePlus.UI.Converters
+{
+    using ViewModels;
+
+    [ValueConversion(typeof(ObservableCollection<CommandViewModel>), typeof(string))]
+    public class CommandsToVisibleCountString : ConverterMarkupExtension
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ObservableCollection<CommandViewModel> commands))
+                return Binding.DoNothing;
+
+            var visibleCount = commands.Where(c => c.IsVisible).Count();
+
+            return visibleCount.ToString();
+        }
+    }
+}
