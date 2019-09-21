cad33384f1145e754a1f95e585dcf242dc5ab412
Yann Duran <yd@live.com.au>
2019-09-21 22:08:40 +1000

    Add VisualStudioService

---
+++ src/ui/Interfaces/IVisualStudioService.cs
@@ -0,0 +1,11 @@
+﻿namespace StartPagePlus.UI.Interfaces
+{
+    public interface IVisualStudioService
+    {
+        void ExecuteCommand(string Action);
+
+        void ShowMessage(string message);
+
+        void NavigateTo(string url, bool internalBrowser);
+    }
+}
