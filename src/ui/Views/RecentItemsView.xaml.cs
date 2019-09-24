365197429be5ec4cef892f31f30e992734dd56ae
Yann Duran <yd@live.com.au>
2019-09-24 14:39:20 +1000

    Add 'clear filter' button

--- src/ui/Views/RecentItemsView.xaml.cs
+++ src/ui/Views/RecentItemsView.xaml.cs
@@ -9,6 +9,7 @@ namespace StartPagePlus.UI.Views
 
     public partial class RecentItemsView : UserControl
     {
+
         public RecentItemsView()
         {
             InitializeComponent();
@@ -19,8 +20,6 @@ namespace StartPagePlus.UI.Views
 
             DataContext = viewModel;
 
-            //https://joshsmithonwpf.wordpress.com/2007/06/12/searching-for-items-in-a-listbox/
-
             var view = (ListCollectionView)CollectionViewSource.GetDefaultView(viewModel.Items);
 
             view.GroupDescriptions.Add(new PropertyGroupDescription("PeriodType"));
@@ -28,11 +27,13 @@ namespace StartPagePlus.UI.Views
             view.SortDescriptions.Add(new SortDescription("PeriodType", ListSortDirection.Ascending));
             view.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Descending));
 
-            var filterText = "";
+            //https://joshsmithonwpf.wordpress.com/2007/06/12/searching-for-items-in-a-listbox/
 
             view.Filter = (object obj) =>
             {
-                if (string.IsNullOrEmpty(filterText))
+                //var filterText = "";
+
+                if (string.IsNullOrEmpty(FilterTextBox.Text))
                     return true;
 
                 var item = obj as RecentItemViewModel;
@@ -41,19 +42,25 @@ namespace StartPagePlus.UI.Views
                 if (string.IsNullOrEmpty(name))
                     return false;
 
-                var index = name.IndexOf(filterText, 0, StringComparison.InvariantCultureIgnoreCase);
+                var index = name.IndexOf(FilterTextBox.Text, 0, StringComparison.InvariantCultureIgnoreCase);
 
                 return (index > -1);
             };
 
             FilterTextBox.TextChanged += (object sender, TextChangedEventArgs e) =>
             {
-                filterText = FilterTextBox.Text;
+                //filterText = FilterTextBox.Text;
                 view.Refresh();
             };
 
             RecentItemsListView.SelectionChanged += (sender, e)
                 => RecentItemsListView.SelectedItem = null;
+        }
+
+        private void ClearFilterText_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FilterTextBox.Text = "";
+            FilterTextBox.Focus();
         }
     }
 }
