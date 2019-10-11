5390e381c84fc9e0bf11a8879f98034da92f149c
Yann Duran <yd@live.com.au>
2019-10-11 19:48:11 +1100

    Add DateToString method
    Related Work Items: #1037, #1062

---
+++ src/core/DateMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+namespace Luminous.Code.Dates
+{
+    public static class DateMethods
+    {
+        public static string DateToString(DateTime dateValue, string format, CultureInfo culture = null)
+            => dateValue.ToString(format, culture ?? CultureInfo.CurrentCulture);
+    }
+}
