58dbb7535dcc0473eacd39a60094fa8c3f343f79
Yann Duran <yd@live.com.au>
2019-10-11 20:00:13 +1100

    Add DateToStringConverter
    Related Work Items: #1037, #1062

---
+++ src/ui/Converters/DateToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using static Luminous.Code.Dates.DateMethods;
+
+namespace StartPagePlus.UI.Converters
+{
+    using MarkupExtensions;
+
+    // usage:
+    // xmlns:c="clr-namespace:StartPagePlus.UI.Converters"
+
+    //Text="{Binding Date, Converter={c:DateToStringConverter Format=d}}"
+
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateToStringConverter : ValueConverterMarkupExtension
+    {
+        public string Format { get; set; } = "d";
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => !(value is DateTime dateValue)
+                ? value
+                : DateToString(dateValue, Format);
+    }
+}
