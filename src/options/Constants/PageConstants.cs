385f07d3aea5610cde7e7343443c7dd9eaaea95b
Yann Duran <yd@live.com.au>
2020-04-28 12:12:09 +1000

    Add new constants
    #1103

--- src/options/Constants/PageConstants.cs
+++ src/options/Constants/PageConstants.cs
@@ -6,9 +6,11 @@
     {
         public const string H1 = "1." + Space;
         public const string H2 = "2." + Space;
+        public const string H3 = "3." + Space;
         public const string Feature = "Feature";
         public const string Features = "Features";
         public const string FeatureSet = "Feature Set";
+        public const string Settings = "Settings";
         public const string Enable = "Enable";
         public const string Enabled = "Enabled";
         public const string EnablesDisables = "Enables/disables";
@@ -16,8 +18,9 @@
 
         public const string PackageName = Vsix.Name;
         public const string PackageFeatureSet = PackageName + Space + FeatureSet;
+        public const string EnableStartPagePlusOptions = "Start Page+ Options";
         public const string PackageVersion = "Version Number";
         public const string OpenLinksInVS = "Open Links in VS";
-        public const string EnableStartPagePlusOptions = "Start Page+ Options";
+        public const string MaxWidth = "Max Width";
     }
 }
