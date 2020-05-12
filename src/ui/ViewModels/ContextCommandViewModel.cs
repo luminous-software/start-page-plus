9f69716ff68d63b5cf2bf55b9a8ca3d417da163a
Yann Duran <yd@live.com.au>
2020-05-12 12:23:25 +1000

    Add classes to support `RecentItemsListView` context menu

---
+++ src/ui/ViewModels/ContextCommandViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.Imaging.Interop;
+
+namespace StartPagePlus.UI.ViewModels
+{
+    public class ContextCommandViewModel : CommandViewModel
+    {
+        private ImageMoniker moniker;
+
+        public ImageMoniker Moniker
+        {
+            get => moniker;
+            set => Set(ref moniker, value);
+        }
+    }
+}
