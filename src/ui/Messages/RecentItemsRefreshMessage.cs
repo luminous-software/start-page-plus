d27802cd239ad60f7d16dbb47d92d1977a95d10d
Yann Duran <yd@live.com.au>
2020-05-27 19:43:41 +1000

    Add a new message to tell view to refresh
    Related Work Items: #1203

---
+++ src/ui/Messages/RecentItemsRefreshMessage.cs
@@ -0,0 +1,10 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+
+namespace StartPagePlus.UI.Messages
+{
+    public class RecentItemsRefreshMessage : MessageBase
+    {
+        public RecentItemsRefreshMessage() : base(null)
+        { }
+    }
+}
