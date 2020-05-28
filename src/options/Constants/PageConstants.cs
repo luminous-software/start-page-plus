1d8e3715084996dacd9fb50a913540a67351b23d
Yann Duran <yd@live.com.au>
2020-05-28 22:27:53 +1000

    Add `StartTabTitleText` setting option
    #1216

--- src/options/Constants/PageConstants.cs
+++ src/options/Constants/PageConstants.cs
@@ -18,14 +18,27 @@
 
         public const string PackageName = Vsix.Name;
         public const string PackageFeatureSet = PackageName + Space + FeatureSet;
-        public const string EnableStartPagePlusOptions = "Start Page+ Options";
-        public const string PackageVersion = "Version Number";
-        public const string OpenLinksInVS = "Open Links in VS";
 
-        public const string ShowRecentItemExtensions = "Show Recent Item Extensions";
-        public const string ShowStartTabTitle = "Show Start Tab Title";
-        public const string MaxWidth = "Max Width";
-        public const string NewsItemsToDisplay = "News Items To Display";
-        public const string RecentItemsToDisplay = "Recent Items To Display";
+        public const string EnableStartPagePlusOptions = "Start Page+ Options";
+        public const string OpenLinksInVS = "Open Links in VS";
+        public const string PackageVersion = "Version Number";
+
+        public const string MaxWidthDisplayName = "Max Width";
+        public const string MaxWidthDescription = "Sets the max width of contents of the Start Page+ window";
+
+        public const string NewsItemsToDisplayDisplayName = "News Items To Display";
+        public const string NewsItemsToDisplayDescription = "Sets the number of items to display in the 'Developer News' list";
+
+        public const string RecentItemsToDisplayDisplayName = "Recent Items To Display";
+        public const string RecentItemsToDisplayDescription = "Sets the number of items to display in the 'Recent Items' list";
+
+        public const string ShowRecentItemExtensionsDisplayName = "Show Recent Item Extensions";
+        public const string ShowRecentItemExtensionsDescription = "Sets the visibility of a project/solution's extension in the 'Recent Items' list";
+
+        public const string ShowStartTabTitleDisplayName = "Show Start Tab Title";
+        public const string ShowStartTabTitleDescription = "Sets the visibility of the title in the 'Start' tab";
+
+        public const string StartTabTitleTextDisplayName = "Start Tab Title Text";
+        public const string StartTabTitleTextDescription = "Sets the text of the 'Start' tab title";
     }
 }
--- src/options/Models/SettingOptions.cs
+++ src/options/Models/SettingOptions.cs
@@ -9,28 +9,33 @@ namespace StartPagePlus.Options.Models
     public class SettingOptions : BaseOptionModel<SettingOptions>
     {
         [Category(Settings)]
-        [DisplayName(Constants.PageConstants.MaxWidth)]
-        [Description("Sets the max width of the window's contents")]
+        [DisplayName(MaxWidthDisplayName)]
+        [Description(MaxWidthDescription)]
         public int MaxWidth { get; set; } = 1175;
 
         [Category(Settings)]
-        [DisplayName(Constants.PageConstants.NewsItemsToDisplay)]
-        [Description("Sets the number of items to display in the 'Developer News' list")]
+        [DisplayName(NewsItemsToDisplayDisplayName)]
+        [Description(NewsItemsToDisplayDescription)]
         public int NewsItemsToDisplay { get; set; } = 10;
 
         [Category(Settings)]
-        [DisplayName(Constants.PageConstants.RecentItemsToDisplay)]
-        [Description("Sets the number of items to display in the 'Recent Items' list")]
+        [DisplayName(RecentItemsToDisplayDisplayName)]
+        [Description(RecentItemsToDisplayDescription)]
         public int RecentItemsToDisplay { get; set; } = 50;
 
         [Category(Settings)]
-        [DisplayName(Constants.PageConstants.ShowRecentItemExtensions)]
-        [Description("Hide a project/solution's extension in the 'Recent Items' list")]
-        public bool ShowRecentItemExtensions { get; set; } = true;
+        [DisplayName(ShowRecentItemExtensionsDisplayName)]
+        [Description(ShowRecentItemExtensionsDescription)]
+        public bool ShowRecentItemExtensions { get; set; } = false;
 
         [Category(Settings)]
-        [DisplayName(Constants.PageConstants.ShowStartTabTitle)]
-        [Description("Allow the title displayed in the Start Tab title to be shown or hidden")]
+        [DisplayName(ShowStartTabTitleDisplayName)]
+        [Description(ShowStartTabTitleDescription)]
         public bool ShowStartTabTitle { get; set; } = true;
+
+        [Category(Settings)]
+        [DisplayName(StartTabTitleTextDisplayName)]
+        [Description(StartTabTitleTextDescription)]
+        public string StartTabTitleText { get; set; } = "What would you like to do today?";
     }
 }
