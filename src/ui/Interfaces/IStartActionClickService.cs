6d32b82d3b5853306664b606400e79c275162786
Yann Duran <yd@live.com.au>
2020-06-07 22:28:49 +1000

    refactor: Add methods to `IStartActionClickService`

--- src/ui/Interfaces/IStartActionClickService.cs
+++ src/ui/Interfaces/IStartActionClickService.cs
@@ -1,5 +1,17 @@
 ﻿namespace StartPagePlus.UI.Interfaces
 {
     public interface IStartActionClickService
-    { }
+    {
+        bool OpenWebPage(string url, bool openInVS);
+
+        bool CreateNewProject();
+
+        bool CloneOrCheckoutCode();
+
+        bool OpenFolder();
+
+        bool OpenProject();
+
+        bool RestartVisualStudio(bool confirm, bool elevated);
+    }
 }
